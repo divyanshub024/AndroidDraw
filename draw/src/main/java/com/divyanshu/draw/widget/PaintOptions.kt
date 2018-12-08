650228caca63600a2e19a405921c53ddda08fd5d
Divyanshu Bhargava <divyanshub024@gmail.com>
2018-12-08 19:02:27 +0530

    erase feature on click

--- draw/src/main/java/com/divyanshu/draw/widget/PaintOptions.kt
+++ draw/src/main/java/com/divyanshu/draw/widget/PaintOptions.kt
@@ -2,4 +2,4 @@ package com.divyanshu.draw.widget
 
 import android.graphics.Color
 
-data class PaintOptions(var color: Int = Color.BLACK, var strokeWidth: Float = 8f, var alpha: Int = 255)
+data class PaintOptions(var color: Int = Color.BLACK, var strokeWidth: Float = 8f, var alpha: Int = 255, var isEraserOn: Boolean = false)
