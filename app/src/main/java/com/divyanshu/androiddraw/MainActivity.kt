e444c9674cefc4d1ff7335afc6d43464a49ab8f2
ChintanRathod <rathod.chintan.h@gmail.com>
2019-06-10 19:39:44 +0530

    Migrated from Support to AndroidX
    - Replaced older support dependencies with AndroidX
    - Simplify dependencies

--- app/src/main/java/com/divyanshu/androiddraw/MainActivity.kt
+++ app/src/main/java/com/divyanshu/androiddraw/MainActivity.kt
@@ -9,11 +9,11 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import android.util.Log
 import android.view.WindowManager
 import android.widget.EditText
